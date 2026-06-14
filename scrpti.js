fb8345defaa693b95f38e91cfd1f03d604208226
muhdy12 <muhdaa.usman1@gmail.com>
2026-06-14 21:24:12 +0000

    Add Airtime purchase and transaction viewing functions

---
+++ scrpti.js
@@ -0,0 +1,20 @@
+<script>
+
+function buyAirtime(){
+let network=document.getElementById("network").value;
+let phone=document.getElementById("phone").value;
+let amount=document.getElementById("amount").value;
+
+document.getElementById("result").innerHTML=
+"An karɓi odar Airtime na "+amount+
+" CFA zuwa "+phone+" ("+network+")";
+}
+
+function viewTransactions(){
+alert("Admin Transactions Page");
+}
+
+</script>
+
+</body>
+</html>
